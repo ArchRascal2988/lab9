4656bb7730d43f47034c775b650bc978376b7481
Lex Matthews <tanukiguernica@gmail.com>
2024-04-12 16:36:36 -0400

    implemented hash table

--- lab9.c
+++ lab9.c
@@ -11,13 +11,14 @@ struct RecordType
 // Fill out this structure
 struct HashType
 {
-
+	struct RecordType* record;
+	struct HashType* next;
 };
 
 // Compute the hash function
-int hash(int x)
+int hash(int x, int sz)
 {
-
+	return x % sz;
 }
 
 // parses input file to an integer array
@@ -73,13 +74,22 @@ void printRecords(struct RecordType pData[], int dataSz)
 // skip the indices which are free
 // the output will be in the format:
 // index x -> id, name, order -> id, name, order ....
-void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+void displayRecordsInHash(struct HashType **pHashArray, int hashSz)
 {
 	int i;
-
+	printf("\nHash Records:\n");
 	for (i=0;i<hashSz;++i)
 	{
 		// if index is occupied with any records, print all
+		struct HashType* temp = pHashArray[i];
+
+		while(temp->record != NULL){
+			printf("%d %c %d\n", temp->record->id, temp->record->name, temp->record->order);
+			if(temp->next == NULL)
+				break;
+			
+			temp = temp->next;
+		}
 	}
 }
 
@@ -88,7 +98,51 @@ int main(void)
 	struct RecordType *pRecords;
 	int recordSz = 0;
 
-	recordSz = parseData("input.txt", &pRecords);
+	recordSz = parseData("./input.txt", &pRecords);
 	printRecords(pRecords, recordSz);
+
 	// Your hash implementation
+	FILE* inFile = fopen("./input.txt", "r");
+	int dataSz;
+	int i, n1, n2, index;
+	char c;
+	struct HashType **hashTable;
+
+	if (inFile)
+	{
+		fscanf(inFile, "%d\n", &dataSz);
+		int tblSz= dataSz * dataSz;
+		hashTable = (struct HashType**) malloc(sizeof(struct HashType) * tblSz);
+		
+		
+		if (hashTable == NULL){
+			printf("Cannot allocate memory\n");
+			exit(-1);
+		}
+
+		for(i = 0; i < tblSz; i++){
+			hashTable[i] = (struct HashType*) malloc(sizeof(struct HashType));
+			hashTable[i]->record = NULL;
+			hashTable[i]->next = NULL;
+		}
+
+		for (i = 0; i < dataSz; ++i)
+		{
+			fscanf(inFile, "%d %c %d", &n1, &c, &n2);
+			index = hash(n1, tblSz);
+			if(hashTable[index]->record != NULL){
+				struct HashType* temp = (struct HashType*) malloc(sizeof(struct HashType));
+				temp->next = hashTable[index];
+				hashTable[index] = temp;
+			} 
+			
+			hashTable[index]->record = (struct RecordType*) malloc(sizeof(struct RecordType));
+			hashTable[index]->record->id = n1;
+			hashTable[index]->record->name = c;
+			hashTable[index]->record->order = n2;
+		}
+
+		displayRecordsInHash(hashTable, tblSz);
+		fclose(inFile);
+	}
 }
